7e1f5a3e476636c83f5dd3073cd61637a6c79c76
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-06 19:19:15 +0700

    write test

---
+++ lib/solver.rb
@@ -0,0 +1,15 @@
+class Solver
+
+    def factorial(input)
+        return nil
+    end
+
+    def reverse(input)
+        return nil
+    end
+
+    def fizzbuzz(input)
+        return nil
+    end
+
+end
