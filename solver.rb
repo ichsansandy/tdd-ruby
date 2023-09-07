d38af28d6895d52994ff5fdf1a1bbfb3326b4223
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-07 15:05:29 +0500

    Factoral method created

--- solver.rb
+++ solver.rb
@@ -1,5 +1,12 @@
 class Solver
-    def factorial
+    def factorial(arg1)
+        raise ArgumentError, "Input must be a non-negative integer" if n < 0
+        return 1 if n == 0
+        result = 1
+        (1..arg1).each do |i|
+          result *= i
+        end
+        result
     end
-    
+
 end
