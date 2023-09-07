0fb0a63134dd41372fa9c847adf5e240ef1e1ec3
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-07 15:09:47 +0500

    fizzbuzz method created

--- solver.rb
+++ solver.rb
@@ -11,5 +11,13 @@ class Solver
     def reverse(string)
         string.reverse
     end
-
+    def fizzbuzz(n)
+        if n % 3 === 0
+            "fizz"
+        elsif n % 5 === 0
+            "buzz"
+        elsif n % 3 === 0 && n % 5 === 0
+            "fizzbuzz"   
+        end 
+    end
 end
